5bd0cd05ca655efe0e6afae47b6a0a5a24057359
Nino <ninoambara@Ninos-MacBook-Air.local>
2024-09-03 11:38:02 +0800

    add day

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime
 
 def hit_api():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now().strftime("%A, %Y-%m-%d %H:%M:%S")
     print(f"[{current_time}] Status Code: hi, Response: hi")
     
 
